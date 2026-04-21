ee128c19302aa32a8796d520072f38ed7002322b
samihabib1986-collab <samihabib1986@gmail.com>
2026-04-21 23:10:22 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ from telegram.ext import (
     MessageHandler,
     filters
 )
-MONGO_URL = os.environ.get("MONGO_URL")
+MONGO_URL = os.environ.get("mongodb+srv://samihabib1986_db_user:59KdWSzGF2V5wHCp@cluster0.bm9w0u0.mongodb.net/?appName=Cluster0")
 
 if not MONGO_URL:
     raise ValueError("MONGO_URL is missing")
