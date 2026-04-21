6c888acaed1c2fa9b10e3ab02d0fa1a0cd244efb
samihabib1986-collab <samihabib1986@gmail.com>
2026-04-21 22:29:05 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ from telegram.ext import (
 # ================== إعدادات ==================
 MONGO_URL = os.environ.get("mongodb+srv://samihabib1986_db_user:a5c7t6@cluster0.bm9w0u0.mongodb.net/?appName=Cluster0")
 TOKEN = os.environ.get("TOKEN")
-
+print(users.find_one())
 if not TOKEN:
     raise ValueError("TOKEN is missing")
 
