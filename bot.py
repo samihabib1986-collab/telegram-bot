76cdd69e6576a3d56e763d70fa9bb753c13a3cac
samihabib1986-collab <samihabib1986@gmail.com>
2026-04-23 11:17:42 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -92,7 +92,18 @@ subjects = {
                     "s1": {
                         "title": "📂 الجهاز الدعامي الحركي",
                         "video": SECTION1_VIDEO,
-                        "questions": section1_questions
+                        "questions": {
+                                "taaleel": [
+                                {
+"question": "23. كثرة التلافيف في قشرة المخ:",
+"options": ["لتقليل حجم الدماغ","لزيادة مساحة سطح القشرة المخية (المادة الرمادية)",""],
+"answer": "لزيادة مساحة سطح القشرة المخية (المادة الرمادية)"
+},
+
+                                            ],
+    "images": [...],
+    "where": [...],
+}
                     },
                     "s2": {
                         "title": "📂 الجهاز العصبي",
@@ -151,12 +162,24 @@ async def approve(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=user_id, text="تم التفعيل /start")
 
 # ================== إرسال سؤال ==================
+if data.startswith("quiz_"):
+    quiz_type = data.replace("quiz_", "")
+
+    user_data[user_id]["quiz_type"] = quiz_type
+    user_data[user_id]["q_index"] = 0
+
+    await send_question(update, context)
+    return
 async def send_question(update, context):
     query = update.callback_query
     user_id = query.from_user.id
 
     data = user_data[user_id]
-    q_list = subjects["science"]["units"][data["unit"]]["sections"][data["section"]]["questions"]
+    unit = user_data[user_id]["unit"]
+sec = user_data[user_id]["section"]
+q_type = user_data[user_id].get("quiz_type", "s1")
+
+q_list = subjects["science"]["units"][unit]["sections"][sec]["questions"]["questions"][q_type]
 
     index = data["q_index"]
 
@@ -244,9 +267,23 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
         return
 
     # بدء الاختبار
-    if data == "start_quiz":
-        await send_question(update, context)
-        return
+if data == "start_quiz":
+
+    keyboard = [
+        [InlineKeyboardButton("🧠 تعاليل", callback_data="quiz_taaleel")],
+        [InlineKeyboardButton("🖼 صور", callback_data="quiz_images")],
+        [InlineKeyboardButton("📍 أين", callback_data="quiz_where")],
+        [InlineKeyboardButton("🔁 ترتيب", callback_data="quiz_level")],
+        [InlineKeyboardButton("⚙️ وظيفة", callback_data="quiz_function")],
+        [InlineKeyboardButton("📊 نتائج", callback_data="quiz_result1")]
+    ]
+
+    await context.bot.send_message(
+        chat_id=query.message.chat_id,
+        text="اختر نوع الأسئلة:",
+        reply_markup=InlineKeyboardMarkup(keyboard)
+    )
+    return
 
     # إجابة
     q_data = user_data[user_id]
